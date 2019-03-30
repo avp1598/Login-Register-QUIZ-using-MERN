37716dae90ebed47df986f180063906533c27644
avp <nakulparmar15@gmail.com>
2019-03-31 00:58:49 +0530

    changes

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/auth_sample'
+    'mongoUrl' : 'mongodb://localhost:27017/auth_sample',
+    'port' : '3000'
 }
